98a49c9e195b56c44f7faf13d789e878b9d7e10c
Frankva <9905928@gmail.com>
2023-02-28 11:19:47 +0100

    migration file

---
+++ orif/timbreuse/Database/Migrations/2023-02-24-152318_AddlogSync.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddlogSync extends Migration
+{
+	public function up()
+	{
+		$field['date']['type'] = 'DATETIME';
+		$field['date']['null'] = false;
+
+		$field['id_badge']['type'] = 'BIGINT';
+		$field['id_badge']['null'] = true;
+
+		$field['inside']['type'] = 'TINYINT';
+		$field['inside']['null'] = false;
+
+		$field['id_log']['type'] = 'INT';
+		$field['id_log']['auto_increment'] = true;
+		$field['id_log']['null'] = false;
+
+		$field['id_user']['type'] = 'INT';
+		$field['id_user']['null'] = false;
+
+		$field['date_badge']['type'] = 'DATETIME';
+		$field['id_user']['null'] = true;
+
+		$field[0] = 'date_modif DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP';
+
+		$field['date_delete']['type'] = 'DATETIME';
+		$field['date_delete']['null'] = true;
+
+		$field['id_ci_user']['type'] = 'INT';
+		$field['id_ci_user']['null'] = true;
+
+		$this->forge->addField($field);
+		$this->forge->addPrimaryKey('id_log');
+		$this->forge->addForeignKey('id_badge', 'badge_sync', 'id_badge');
+		$this->forge->addForeignKey('id_user', 'user_sync', 'id_user');
+        $this->forge->createTable('log_sync');
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('log_sync');
+	}
+}
--- orif/user/Database/Migrations/2021-03-19-090829_add_user.php
+++ orif/user/Database/Migrations/2021-03-19-090829_add_user.php
@@ -44,7 +44,7 @@ class AddUser extends \CodeIgniter\Database\Migration
         ]);
         $this->forge->addKey('id',true);
         $this->forge->addForeignKey('fk_user_type','user_type','id');
-        $this->forge->createTable('user',true);
+        $this->forge->createTable('ci_user',true);
         $seeder=\Config\Database::seeder();
         $seeder->call('\User\Database\Seeds\AddUserDatas');
     }
@@ -54,6 +54,6 @@ class AddUser extends \CodeIgniter\Database\Migration
      */
     public function down()
     {
-        $this->forge->dropTable('user');
+        $this->forge->dropTable('ci_user');
     }
 }
