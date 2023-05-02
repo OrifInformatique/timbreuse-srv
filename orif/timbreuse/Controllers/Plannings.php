772556767ac91e1a8d6e78616a5cc0ce71e16f2b
Frankva <9905928@gmail.com>
2023-05-02 15:21:59 +0200

    edit planning formular

--- orif/timbreuse/Controllers/Plannings.php
+++ orif/timbreuse/Controllers/Plannings.php
@@ -13,6 +13,51 @@ use CodeIgniter\I18n\Time;
 
 class Plannings extends BaseController
 {
+    private $validateArray = [
+            'planningId' => 'required|integer',
+            'dueHoursMonday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[10]|required',
+            'dueMinutesMonday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[59]|required',
+            'dueHoursTuesday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[10]|required',
+            'dueMinutesTuesday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[59]|required',
+            'dueHoursWednesday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[10]|required',
+            'dueMinutesWednesday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[59]|required',
+            'dueHoursThursday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[10]|required',
+            'dueMinutesThursday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[59]|required',
+            'dueHoursFriday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[10]|required',
+            'dueMinutesFriday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[59]|required',
+            'offeredHoursMonday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[10]|required',
+            'offeredMinutesMonday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[59]|required',
+            'offeredHoursTuesday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[10]|required',
+            'offeredMinutesTuesday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[59]|required',
+            'offeredHoursWednesday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[10]|required',
+            'offeredMinutesWednesday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[59]|required',
+            'offeredHoursThursday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[10]|required',
+            'offeredMinutesThursday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[59]|required',
+            'offeredHoursFriday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[10]|required',
+            'offeredMinutesFriday' => 'greater_than_equal_to[0]'
+            . '|less_than_equal_to[59]|required',
+        ]; 
+
+
     public function initController(RequestInterface $request,
         ResponseInterface $response, LoggerInterface $logger)
     {
@@ -58,20 +103,110 @@ class Plannings extends BaseController
         return get_ci_user_id();
     }
 
-    public function edit_planning($planningId)
+    public function edit_planning($planningId=null)
     {
+        if (($this->request->getMethod() === 'post')
+            and ($this->validate($this->validateArray))) {
+            return $this->post_edit_planning();
+        }
+        $planningId = $this->request->getPost('planningId') ?? $planningId;
+        if (is_null($planningId)) {
+            return $this->block_ci_user();
+        }
         $model = model(PlanningsModel::class);
         if ((!$model->is_access_ci_user($this->get_ci_user_id(), $planningId))
                 and (!$this->is_admin())) {
             return $this->block_ci_user();
         }
-        $data = $model->get_planning_hours_minutes($planningId);
+        $data = $this->get_planning_hours_minutes_or_old_post($planningId,
+            $model);
         $data['title'] = ucfirst(lang('tim_lang.titlePlanning'));
         $data['h3title'] = ucfirst(lang('tim_lang.titlePlanning'));
         $data['labels'] = $this->get_label_for_edit_planning();
+        $data['planningId'] = $planningId;
+        $data['action'] = '.';
         $this->display_view(['Timbreuse\Views\planning\edit_planning.php'],
             $data);
-   }
+    }
+
+    protected function get_planning_hours_minutes_or_old_post($planningId,
+        $model)
+    {
+        if ($this->request->getMethod() === 'post') {
+            return $this->format_post_old();
+        } else {
+            return $model->get_planning_hours_minutes($planningId);
+        }
+    }
+
+    protected function format_post_old()
+    {
+        $names = $this->get_array_for_format_post_old();
+        $post = $this->request->getPost();
+        $data = array();
+        foreach ($names as $key => $value) {
+            if (isset($data[$value]['hour'])) {
+                $data[$value]['minute'] = $post[$key];
+            } else {
+                $data[$value]['hour'] = $post[$key];
+            }
+        }
+        return $data;
+    }
+
+    protected function get_array_for_format_post_old()
+    {
+        $names = array();
+        $names['dueHoursMonday'] = 'due_time_monday';
+        $names['dueMinutesMonday'] = 'due_time_monday';
+        $names['dueHoursTuesday'] = 'due_time_tuesday';
+        $names['dueMinutesTuesday'] = 'due_time_tuesday';
+        $names['dueHoursWednesday'] = 'due_time_wednesday';
+        $names['dueMinutesWednesday'] = 'due_time_wednesday';
+        $names['dueHoursThursday'] = 'due_time_thursday';
+        $names['dueMinutesThursday'] = 'due_time_thursday';
+        $names['dueHoursFriday'] = 'due_time_friday';
+        $names['dueMinutesFriday'] = 'due_time_friday';
+        $names['offeredHoursMonday'] = 'offered_time_monday';
+        $names['offeredMinutesMonday'] = 'offered_time_monday';
+        $names['offeredHoursTuesday'] = 'offered_time_tuesday';
+        $names['offeredMinutesTuesday'] = 'offered_time_tuesday';
+        $names['offeredHoursWednesday'] = 'offered_time_wednesday';
+        $names['offeredMinutesWednesday'] = 'offered_time_wednesday';
+        $names['offeredHoursThursday'] = 'offered_time_thursday';
+        $names['offeredMinutesThursday'] = 'offered_time_thursday';
+        $names['offeredHoursFriday'] = 'offered_time_friday';
+        $names['offeredMinutesFriday'] = 'offered_time_friday';
+        return $names;
+    }
+
+
+
+    protected function post_edit_planning()
+    {
+        $model = model(PlanningsModel::class);
+        $post = $this->request->getPost();
+        $formatedArray = $this->format_form_array($post);
+        if (isset($post['planningId'])) {
+            $model->update($post['planningId'], $formatedArray);
+        }
+        return redirect()->to(current_url() . '/../../../');
+    }
+
+    protected function format_form_array($formArray)
+    {
+        $names = $this->get_array_for_format_post_old();
+        $formatedArray = array();
+        foreach ($names as $key => $value) {
+            if (isset($formatedArray[$value])) {
+                $formatedArray[$value] .= ':' . $formArray[$key];
+            } else {
+                $formatedArray[$value] = $formArray[$key];
+            }
+        }
+        return $formatedArray;
+
+    }
 
     protected function block_ci_user()
     {
--- orif/timbreuse/Views/planning/edit_planning.php
+++ orif/timbreuse/Views/planning/edit_planning.php
@@ -191,52 +191,52 @@ input:invalid {
             <?= esc($labels['offeredTime']) ?>
         </div>
         <div id="mondayDueTimeInput" class="form-group grid-hour-input grid-input-center" >
-        <input id="dueHoursMonday" class="form-control " type="number" name="dueHoursMonday" list="defaultHours" step="1" min="00" max="10" required value="<?=esc($due_time_monday['hour'])?>">
+        <input id="dueHoursMonday" class="form-control " type="number" name="dueHoursMonday" list="defaultHours" step="1" min="0" max="10" required value="<?=esc($due_time_monday['hour'])?>">
             <span>:</span>
             <input id="dueMinutesMonday" class="form-control" type="number" name="dueMinutesMonday" list="defaultMinutes" step="1" min="0" max="59" required value="<?=esc($due_time_monday['minute'])?>">
         </div>
         <div id="tuesdayDueTimeInput" class="form-group grid-hour-input grid-input-center">
-        <input id="dueHoursTuesday" class="form-control" type="number" name="dueHoursTuesday" list="defaultHours" step="1" min="00" max="10" required value="<?=esc($due_time_tuesday['hour'])?>">
+        <input id="dueHoursTuesday" class="form-control" type="number" name="dueHoursTuesday" list="defaultHours" step="1" min="0" max="10" required value="<?=esc($due_time_tuesday['hour'])?>">
             <span>:</span>
             <input id="dueMinutesTuesday" class="form-control" type="number" name="dueMinutesTuesday" list="defaultMinutes" step="1" min="0" max="59" required value="<?=esc($due_time_tuesday['minute'])?>">
         </div>
         <div id="wednesdayDueTimeInput" class="form-group grid-hour-input grid-input-center">
-        <input id="dueHoursWednesday" class="form-control" type="number" name="dueHoursWednesday" list="defaultHours" step="1" min="00" max="10" required value="<?=esc($due_time_wednesday['hour'])?>">
+        <input id="dueHoursWednesday" class="form-control" type="number" name="dueHoursWednesday" list="defaultHours" step="1" min="0" max="10" required value="<?=esc($due_time_wednesday['hour'])?>">
             <span>:</span>
             <input id="dueMinutesWednesday" class="form-control" type="number" name="dueMinutesWednesday" list="defaultMinutes" step="1" min="0" max="59" required value="<?=esc($due_time_wednesday['minute'])?>">
         </div>
         <div id="thursdayDueTimeInput" class="form-group grid-hour-input grid-input-center">
-        <input id="dueHoursThursday" class="form-control" type="number" name="dueHoursThursday" list="defaultHours" step="1" min="00" max="10" required value="<?=esc($due_time_thursday['hour'])?>">
+        <input id="dueHoursThursday" class="form-control" type="number" name="dueHoursThursday" list="defaultHours" step="1" min="0" max="10" required value="<?=esc($due_time_thursday['hour'])?>">
             <span>:</span>
             <input id="dueMinutesThursday" class="form-control" type="number" name="dueMinutesThursday" list="defaultMinutes" step="1" min="0" max="59" required value="<?=esc($due_time_thursday['minute'])?>">
         </div>
         <div id="fridayDueTimeInput" class="form-group grid-hour-input grid-input-center">
-        <input id="dueHoursFriday" class="form-control" type="number" name="dueHoursFriday" list="defaultHours" step="1" min="00" max="10" required value="<?=esc($due_time_friday['hour'])?>">
+        <input id="dueHoursFriday" class="form-control" type="number" name="dueHoursFriday" list="defaultHours" step="1" min="0" max="10" required value="<?=esc($due_time_friday['hour'])?>">
             <span>:</span>
             <input id="dueMinutesFriday" class="form-control" type="number" name="dueMinutesFriday" list="defaultMinutes" step="1" min="0" max="59" required value="<?=esc($due_time_friday['minute'])?>">
         </div>
         <div id="mondayOfferedTimeInput" class="form-group grid-hour-input grid-input-center">
-            <input id="offeredHoursMonday" class="form-control" type="number" name="offeredHoursMonday" list="defaultHours" step="1" min="00" max="10" required value="<?=esc($offered_time_monday['hour'])?>">
+            <input id="offeredHoursMonday" class="form-control" type="number" name="offeredHoursMonday" list="defaultHours" step="1" min="0" max="10" required value="<?=esc($offered_time_monday['hour'])?>">
             <span>:</span>
             <input id="offeredMinutesMonday" class="form-control" type="number" name="offeredMinutesMonday" list="defaultMinutes" step="1" min="0" max="59" required value="<?=esc($offered_time_monday['minute'])?>">
         </div>
         <div id="tuesdayOfferedTimeInput" class="form-group grid-hour-input grid-input-center">
-            <input id="offeredHoursTuesday" class="form-control" type="number" name="offeredHoursTuesday" list="defaultHours" step="1" min="00" max="10" required value="<?=esc($offered_time_tuesday['hour'])?>">
+            <input id="offeredHoursTuesday" class="form-control" type="number" name="offeredHoursTuesday" list="defaultHours" step="1" min="0" max="10" required value="<?=esc($offered_time_tuesday['hour'])?>">
             <span>:</span>
             <input id="offeredMinutesTuesday" class="form-control" type="number" name="offeredMinutesTuesday" list="defaultMinutes" step="1" min="0" max="59" required value="<?=esc($offered_time_tuesday['minute'])?>">
         </div>
         <div id="wednesdayOfferedTimeInput" class="form-group grid-hour-input grid-input-center">
-            <input id="offeredHoursWednesday" class="form-control" type="number" name="offeredHoursWednesday" list="defaultHours" step="1" min="00" max="10" required value="<?=esc($offered_time_wednesday['hour'])?>">
+            <input id="offeredHoursWednesday" class="form-control" type="number" name="offeredHoursWednesday" list="defaultHours" step="1" min="0" max="10" required value="<?=esc($offered_time_wednesday['hour'])?>">
             <span>:</span>
             <input id="offeredMinutesWednesday" class="form-control" type="number" name="offeredMinutesWednesday" list="defaultMinutes" step="1" min="0" max="59" required value="<?=esc($offered_time_wednesday['minute'])?>">
         </div>
         <div id="thursdayOfferedTimeInput" class="form-group grid-hour-input grid-input-center">
-            <input id="offeredHoursThursday" class="form-control" type="number" name="offeredHoursThursday" list="defaultHours" step="1" min="00" max="10" required value="<?=esc($offered_time_thursday['hour'])?>">
+            <input id="offeredHoursThursday" class="form-control" type="number" name="offeredHoursThursday" list="defaultHours" step="1" min="0" max="10" required value="<?=esc($offered_time_thursday['hour'])?>">
             <span>:</span>
             <input id="offeredMinutesThursday" class="form-control" type="number" name="offeredMinutesThursday" list="defaultMinutes" step="1" min="0" max="59" required value="<?=esc($offered_time_thursday['minute'])?>">
         </div>
         <div id="fridayOfferedTimeInput" class="form-group grid-hour-input grid-input-center">
-            <input id="offeredHoursFriday" class="form-control" type="number" name="offeredHoursFriday" list="defaultHours" step="1" min="00" max="10" required value="<?=esc($offered_time_friday['hour'])?>">
+            <input id="offeredHoursFriday" class="form-control" type="number" name="offeredHoursFriday" list="defaultHours" step="1" min="0" max="10" required value="<?=esc($offered_time_friday['hour'])?>">
             <span>:</span>
             <input id="offeredMinutesFriday" class="form-control" type="number" name="offeredMinutesFriday" list="defaultMinutes" step="1" min="0" max="59" required value="<?=esc($offered_time_friday['minute'])?>">
         </div>
@@ -246,23 +246,23 @@ input:invalid {
         </div>
 
         <datalist id="defaultHours">
+          <option value="0">
+          <option value="1">
           <option value="2">
-          <option value="3">
           <option value="4">
-          <option value="5">
-          <option value="6">
           <option value="7">
           <option value="8">
         </datalist>
 
         <datalist id="defaultMinutes">
-          <option value="00">
-          <option value="06">
+          <option value="0">
+          <option value="6">
           <option value="12">
           <option value="15">
           <option value="30">
           <option value="45">
         </datalist>
+        <input type="hidden" name="planningId" value="<?=esc($planningId)?>">
 
     </form>
 </section>
