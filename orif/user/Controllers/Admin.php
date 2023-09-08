4f437c4c2db177dfcb10923387bf974c86ee947b
Didier Viret <didier.viret@orif.ch>
2023-09-08 11:15:55 +0200

    Fix type error in password_change_user

--- orif/user/Controllers/Admin.php
+++ orif/user/Controllers/Admin.php
@@ -197,7 +197,7 @@ class Admin extends BaseController
      * @param integer $user_id = ID of the user to update
      * @return void
      */
-    public function password_change_user(int $user_id): Response
+    public function password_change_user(int $user_id): string|Response
     {
         // Get user from DB, redirect if user doesn't exist
         $user = $this->user_model->withDeleted()->find($user_id);
