7d2967e263fb495867c85707071d0258959b946f
Frankva <9905928@gmail.com>
2024-01-15 16:47:57 +0100

    add roundabout for security issue

--- app/Config/Boot/production.php
+++ app/Config/Boot/production.php
@@ -7,7 +7,13 @@
  | Don't show ANY in production environments. Instead, let the system catch
  | it and display a generic error message.
  */
-ini_set('display_errors', '0');
+# ini_set('display_errors', '0');
+ini_set('display_errors', 'Off'); # Workarounds for CodeIgniter4 vulnerable to
+# information disclosure when detailed error report is displayed in production
+# environment 
+# https://github.com/OrifInformatique/ci_packbase_v4/security/dependabot/2
+# When we update CodeIgniter to 4.4.3 or upper, change Off to 0
+
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 
 /*
