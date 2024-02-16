6091168db4ffde676e35fc7ae5f6807d5c5aa446
ovsiankina <ovsiankina@proton.me>
2024-02-16 08:35:59 +0100

    Codeigniter update to 4.4.5

--- app/Config/Boot/production.php
+++ app/Config/Boot/production.php
@@ -6,15 +6,15 @@
  |--------------------------------------------------------------------------
  | Don't show ANY in production environments. Instead, let the system catch
  | it and display a generic error message.
+ |
+ | If you set 'display_errors' to '1', CI4's detailed error report will show.
  */
-# ini_set('display_errors', '0');
-ini_set('display_errors', 'Off'); # Workarounds for CodeIgniter4 vulnerable to
+ini_set('display_errors', '0');
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 # information disclosure when detailed error report is displayed in production
 # environment 
 # https://github.com/OrifInformatique/ci_packbase_v4/security/dependabot/2
 # When we update CodeIgniter to 4.4.3 or upper, change Off to 0
-
-error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 
 /*
  |--------------------------------------------------------------------------
