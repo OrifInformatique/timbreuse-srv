e4a32b602fdd06d4dec72ad37ed7662069a1ae02
Didier Viret <didier.viret@orif.ch>
2024-06-06 09:17:18 +0200

    Refactor user access level check

--- orif/timbreuse/Controllers/Home.php
+++ orif/timbreuse/Controllers/Home.php
@@ -15,20 +15,13 @@ class Home extends BaseController
         $this->session=\Config\Services::session();
     }
 
-    private function is_connected_user()
-    {
-        return session()->get('user_access')
-            >= config('\User\Config\UserConfig')->access_lvl_registered;
-    }
-
 	public function index() {
-        if ($this->is_connected_user()) {
+        // Check user access level
+        if (session()->get('user_access') >= config('\User\Config\UserConfig')->access_lvl_registered) {
             return redirect()->to(base_url() . 'PersoLogs/perso_time');
-        }
-
-        if (session()->get('user_access') == config('\User\Config\UserConfig')->access_lvl_guest) {
+        } elseif (session()->get('user_access') == config('\User\Config\UserConfig')->access_lvl_guest) {
+            // Guest users are not managed in this application
             $data['message'] = lang('tim_lang.403_error_message');
-
             return $this->display_view('User\Views\errors\403error', $data);
         } else {
             return redirect()->to('user/auth/login');
